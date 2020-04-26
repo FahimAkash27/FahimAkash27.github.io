bce7a7db1fe795228ab38467716c6f2448a46ce8
FahimAkash27 <gitakash27@gmail.com>
2020-04-26 16:10:57 +0600

    First

---
+++ js/friend.js
@@ -0,0 +1,43 @@
+function FriendCheck(){
+
+  var question1 = document.friendsquiz.q1.value;
+
+  var questions = [document.friendsquiz.q1.value,document.friendsquiz.q2.value,document.friendsquiz.q3.value,
+                  document.friendsquiz.q4.value,document.friendsquiz.q5.value,document.friendsquiz.q6.value,
+                  document.friendsquiz.q7.value,document.friendsquiz.q8.value,document.friendsquiz.q9.value,
+                  document.friendsquiz.q10.value,document.friendsquiz.q11.value,document.friendsquiz.q12.value,
+                  document.friendsquiz.q13.value,document.friendsquiz.q14.value,document.friendsquiz.q15.value,
+                  document.friendsquiz.q16.value,document.friendsquiz.q17.value,document.friendsquiz.q18.value,
+                  document.friendsquiz.q19.value,document.friendsquiz.q20.value];
+
+    var qno = 1;              
+    var pb = 101;
+    var count = 0;
+
+    for(var i = 0; i<20; i++){
+
+      if(questions[i] == "1"){
+        var element = document.getElementById(qno);
+        element.style.visibility = "visible";
+        element.style.backgroundColor = "forestgreen";
+        document.getElementById(pb).innerHTML = "Right"
+        qno++;
+        pb++;
+        count++;
+      }else{
+        var element = document.getElementById(qno);
+        element.style.visibility = "visible";
+        element.style.backgroundColor = "firebrick";
+        document.getElementById(pb).innerHTML = "Wrong"
+        qno++;
+        pb++;
+      }
+    }
+
+    document.getElementById("scorediv").style.visibility = "visible";
+    document.getElementById("score").innerHTML = count;
+
+    document.getElementById("buttonli").style.visibility = "hidden"
+
+
+}
